d554c77145c35366cae0b3f9846fcf4c03f6954a
nileshacademic@gmail.com <nileshacademic@gmail.com>
2020-07-25 13:08:37 +0000

    longestsentencelength

---
+++ pythontestdrivendevelopment/longestsentencelength.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+
+S="How are you. I am quite good"
+
+def solution(S):
+    c=S.split(".")
+    print (c)
+
+solution(S)
+
+find longest senetence in a string separated by dot
